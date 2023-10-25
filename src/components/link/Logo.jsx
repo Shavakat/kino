283d2211dcc9b073a5472ba68f60958628d0e907
Shavakat <karimov2406s@mail.ru>
2023-10-25 08:42:42 +0300

    25.10.2023 10:42

--- src/components/link/Logo.jsx
+++ src/components/link/Logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Logo.css'
 
 const Logo = () => {
-  return (<>
-      <img src="src/assets/Logo.svg" alt="" />
+  return <>
+  <div id="logo">
+    <img id='logo_img' src="src/assets/Logo.svg" alt="" />
+    <img src="src/assets/Kinoarea.svg" alt="" />
+  </div>
   </>
-
-  )
 }
 
 export default Logo
