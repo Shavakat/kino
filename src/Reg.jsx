a32a396c135026708c7cc494307898723b1d5bee
Shavkat <karimov2406s@mail.ru>
2023-10-28 15:08:06 +0300

    reg

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -1,9 +1,25 @@
 import { useState, useSyncExternalStore } from 'react'
+import Logo from './components/link/Logo.jsx'
 import './Reg.css'
+import Input from './LoginComponents/inputs/Input'
+import Button from './LoginComponents/button/ButtonYellow.jsx'
 
 function Reg() {
   return ( <>
-  
+  <div className='Login_item'>
+      <Logo />
+      <h1 id='title_vhod'>Зарегистрироваться</h1>
+      <div className="inptu">
+        <Input placeholder={'Имя'} type={'text'} />
+        <Input placeholder={'Фамилия'} type={'text'} />
+        <Input placeholder={'Придумайте логин'} type={'text'} />
+        <Input placeholder={'Придумайте пароль'} type={'password'} />
+        <Input placeholder={'Повторите пароль'} type={'password'} />
+        <Input placeholder={'Номер телефона или e-mail'} type={'tel'} />
+        <Button text={'Зарегистрироваться'}/>
+      </div>
+    </div>
+
   </>
     
   )
