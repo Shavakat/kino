2cedda4433aacdf179c287641d1bb80544b7303e
John Doe <karimov2406s@mail.ru>
2023-10-31 16:10:14 +0500

    123

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AllFilm from './components/button/AllFilm'
 import TrailerTitle from './components/titleCont/TrailerTitle.jsx'
 import Movies from './components/movies/Movies'
 import TrailerUnder from './components/titleCont/TrailerUnder'
+import Moviesmin from './components/movies/Moviesmin'
 
 function App() {
 
@@ -28,6 +29,12 @@ function App() {
       <div className='TT_app'><TrailerTitle /></div>
       <div className="movies_app"><Movies /></div>
       <TrailerUnder/>
+      <div className="moviesmin">
+        <Moviesmin/>
+        <Moviesmin/>
+        <Moviesmin/>
+        <Moviesmin/>
+      </div>
     </>
   )
 }
---
+++ src/components/movies/Moviesmin.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Moviesmin.css'
+
+const Moviesmin = () => {
+    return (
+        <div className="mini_film">
+            <div className="trailer_img_item_min">
+                <img className="trailer_img_play_min" src='src/assets/Play.svg' alt="" />
+                <img className="trailer_img_min" src="src/img/image 3.png" alt="" />
+            </div>
+            <p className="mini_f_name">Форсаж 9</p>
+        </div>
+
+
+
+    )
+}
+
+export default Moviesmin
